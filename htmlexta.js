433e6eb2f187503020bb60a2b23e5cb4be034b5b
radhika-vatti <radhika.vatti@thermofisher.com>
2022-10-24 13:08:22 -0700

    added js file

---
+++ htmlexta.js
@@ -0,0 +1,37 @@
+const newman = require('newman');
+
+newman.run({
+    collection: 'ScaledConfiguratorsAPIwithGit.postman_collection.json', // Collection URL from a public link or the Postman API can also be used
+    reporters: ['htmlextra'],
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            // export: './report.html',
+            // template: './template.hbs'
+            // logs: true,
+             showOnlyFails: true,
+             noSyntaxHighlighting: true,
+            // testPaging: true,
+            // browserTitle: "My Newman report",
+            // title: "My Newman Report",
+            // titleSize: 4,
+            // omitHeaders: true,
+            // skipHeaders: "Authorization",
+            // omitRequestBodies: true,
+            // omitResponseBodies: true,
+            // hideRequestBody: ["Login"],
+            // hideResponseBody: ["Auth Request"],
+            // showEnvironmentData: true,
+            // skipEnvironmentVars: ["API_KEY"],
+            // showGlobalData: true,
+            // skipGlobalVars: ["API_TOKEN"],
+             skipSensitiveData: true,
+            // showMarkdownLinks: true,
+            // showFolderDescription: true,
+            // timezone: "Australia/Sydney",
+            // skipFolders: "folder name with space,folderWithoutSpace",
+            // skipRequests: "request name with space,requestNameWithoutSpace",
+            // displayProgressBar: true
+        }
+    }
+});
